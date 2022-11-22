4eaeeb505b8d0eef6e089cde10c998ee5554c0e9
Kenan KILIC <96704212+Klcknn@users.noreply.github.com>
2022-11-22 11:31:45 +0300

    Finally ProfileCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import KiriImage from "./images/siri.png";
 
 function App() {
     return (
         <div>
-            <div>Personal Digital Assasiant</div>
-            <ProfileCard title="Alexa" text="@alexa99"/>
-            <ProfileCard title="Cortana" text="@cortana32"/>
-            <ProfileCard title="Junel" text="@junel766"/>
+            <section class="hero is-primary">
+                <div class="hero-body">
+                    <p class="title has-text-centered">Profile Card Assasiants</p>
+                    <p class="subtitle has-text-centered has-text-dark">Bölüm 3: Building with Reusable Components</p>
+                </div>
+            </section>
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard 
+                                title="Alexa" 
+                                text="alexa@76" 
+                                image={AlexaImage}
+                                description="Alexa was created by Amazon Web Services and help you buy for your"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard 
+                                title="Cortana" 
+                                text="cortana@26" 
+                                image={CortanaImage} 
+                                description="Cortana was made by Microsoft and help you buy for your"    
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard 
+                                title="Kiri Ass" 
+                                text="kiri@14" 
+                                image={KiriImage} 
+                                description="Kiri wa made by Apple Inc. and being used by Apple Inc" 
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 };
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -6,13 +6,23 @@ function ProfileCard(props) {
     */
    
     // 2. step:
-    const {title, text} = props; 
+    const {title, text , image , description } = props; 
     
     return (
-        <div>        
-            <div>The title is  {title} </div>
-            <div>The text  is  {text}  </div>
-        </div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="Placeholder img" />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4"> {title} </p>
+                    <p className="subtitle is-6"> {text} </p>
+                </div>
+                <div className="content"> {description} </div>
+            </div>
+        </div>        
     );
 };
 
